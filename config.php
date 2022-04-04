9f7e2812985d7c6a031ab4dca0501471e8229474
Nepdesk <nepdesk@gmail.com>
2022-04-04 10:05:19 +0530

    Initial Commit
    Complete User Management System. Animated Epic Login, Register, Reset Password, Email Verification and more PHP Script.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "login";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+$base_url = "http://localhost/animated-php-login-register-system";
+$my_email = "nepdesk@gmail.com";
+
+?>
